1a9065f6256694aa1f0c27e3a8a604d885a73003
Raul Esteban Burro Cespedes <ax5bm10@gmail.com>
2015-03-27 21:28:15 -0400

    configurada la vista del cliente para el buscador, la vista de edicion y las tablas, hay que agregar algunos botones y hacer que funcione con el rest, tb hace falta el sistema de login con rest, el sistema funciona completamente sin coneion con el server

--- app/scripts/controllers/clientes.js
+++ app/scripts/controllers/clientes.js
@@ -7,13 +7,46 @@
  * # ClientesCtrl
  * Controller of the modulosGeneralesApp
  */
+
+function DialogoClienteCtrl ($scope, $mdDialog, client) {
+    $scope.newCliente = client;
+    $scope.hide = function () {
+      $mdDialog.hide();
+    };
+    $scope.cancel = function () {
+      $mdDialog.cancel();
+    };
+    $scope.answer = function (answer) {
+      $mdDialog.hide(answer);
+    };
+  }
+
+
+
 angular.module('modulosGeneralesApp')
-  .controller('ClientesCtrl', function ($scope, clientes) {
+  .controller('ClientesCtrl', function ($scope, clientes, $mdDialog) {
     clientes.get(function (response) {
       $scope.clientes = response.results;
     });
 
-    $scope.cargar = function (client) {
+    /*$scope.cargar = function (client) {
       $scope.newCliente = client;
+    };*/
+
+    $scope.cargar = function(ev,cliente) {
+      console.log('hola');
+      $mdDialog.show({
+        controller: DialogoClienteCtrl,
+        templateUrl: 'views/dialogs/dialogo_cliente.html',
+        targetEvent: ev,
+        locals: {client: cliente},
+        bindToController: true
+      })
+        .then(function(answer) {
+          $scope.alert = 'You said the information was "' + answer + '".';
+        }, function() {
+          $scope.alert = 'You cancelled the dialog.';
+        });
     };
   });
+
