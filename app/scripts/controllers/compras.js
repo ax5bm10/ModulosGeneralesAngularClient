c5cec257f467592b61805f4ddacfb352715328b1
Raul Esteban Burro Cespedes <ax5bm10@gmail.com>
2015-03-29 20:56:15 -0400

    agregamos el modulo de nodewebkit para que se cree la app, elimine por ahora el uglify del gruntfile porque daba error con el angular y no dejaba que funcion el $mdDialog, falta aun la agregacion de los fonts que es lo que esta dando error

--- app/scripts/controllers/compras.js
+++ app/scripts/controllers/compras.js
@@ -9,8 +9,8 @@
  */
 
 
-function DialogController($scope, $mdDialog, cmp) {
-  console.log(cmp);
+/*function DialogController($scope, $mdDialog, cmp) {
+  console.log('entre en el controlodar del dialogo');
   $scope.compra = cmp;
   $scope.hide = function() {
     $mdDialog.hide();
@@ -21,11 +21,10 @@ function DialogController($scope, $mdDialog, cmp) {
   $scope.answer = function(answer) {
     $mdDialog.hide(answer);
   };
-}
-
+}*/
 
 angular.module('modulosGeneralesApp')
-  .controller('ComprasCtrl', function ($scope, compras, $mdDialog) {
+  .controller('ComprasCtrl',['$scope','compras','$mdDialog', function ($scope, compras, $mdDialog) {
 
     compras.get(function (response) {
       $scope.compras = response.results;
@@ -34,7 +33,19 @@ angular.module('modulosGeneralesApp')
     $scope.showAdvanced = function(ev,compra) {
       console.log('hola');
       $mdDialog.show({
-        controller: DialogController,
+        controller: ['$scope','$mdDialog','cmp', function ($scope, $mdDialog, cmp) {
+          console.log('entre en el controlodar del dialogo');
+          $scope.compra = cmp;
+          $scope.hide = function() {
+            $mdDialog.hide();
+          };
+          $scope.cancel = function() {
+            $mdDialog.cancel();
+          };
+          $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+          };
+        }],
         templateUrl: 'views/dialogs/dialog1.html',
         targetEvent: ev,
         locals: {cmp: compra},
@@ -46,5 +57,5 @@ angular.module('modulosGeneralesApp')
           $scope.alert = 'You cancelled the dialog.';
         });
     };
-  });
+  }]);
 
